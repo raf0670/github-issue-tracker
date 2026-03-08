9a584b03d32382c98a94fe042ddfaa66d11485a5
raf0670 <swagbondhu@gmail.com>
2026-03-09 02:29:32 +0600

    improved UX for search option

--- js/home.js
+++ js/home.js
@@ -66,15 +66,24 @@ const displayIssues = (issues) => {
     // get container
     const container = document.getElementById("card-container");
     container.innerHTML = "";
-
+    
     // set total count
     document.getElementById("total-issues").innerText = issues.length;
+    
+    if (issues.length === 0) {
+        container.innerHTML = `
+                    <div id="no-issues" class="col-span-full">
+                        <h1 class="text-center text-4xl text-gray-600">No Issues Found</h1>
+                    </div>
+        `
+        return;
+    }
 
     // create each card
     issues.forEach(issue => {
         const card = document.createElement("div");
         card.innerHTML = `
-            <div id="card-${issue.id}" class="${issue.status === "open" ? `border-t-3 border-t-green-600` : `border-t-3 border-t-purple-600`} h-80 flex flex-col justify-center bg-white shadow-md rounded-lg p-4 space-y-3.5">
+            <div id="card-${issue.id}" class="${issue.status === "open" ? `border-t-3 border-t-green-600` : `border-t-3 border-t-purple-600`} h-full flex flex-col justify-center bg-white shadow-md rounded-lg p-4 space-y-4">
                 <div class="flex justify-between items-center">
                     ${issue.status === "open" ? `<img src="./assets/Open-Status.png" class="" alt="">` : `<img src="./assets/Closed- Status .png" class="" alt="">`}
                     ${issue.priority === "high" ? `<h2 class="bg-red-100 rounded-full w-20 text-center text-xs font-semibold p-1 text-red-600">HIGH</h2>` : issue.priority === "medium" ? `<h2 class="bg-yellow-100 rounded-full w-20 text-center text-xs font-semibold p-1 text-yellow-600">MEDIUM</h2>` : `<h2 class="bg-[#EEEFF2] rounded-full w-20 text-center text-xs font-semibold p-1 text-gray-600">LOW</h2>`}
@@ -94,8 +103,8 @@ const displayIssues = (issues) => {
                 </span>
 
                 <div class="text-[#64748B] text-sm py-2">
-                    <p>${issue.author}</p>
-                    <p>${issue.createdAt}</p>
+                    <p>By ${issue.author}</p>
+                    <p>${issue.createdAt[5]}${issue.createdAt[6]}/${issue.createdAt[8]}${issue.createdAt[9]}/${issue.createdAt[0]}${issue.createdAt[1]}${issue.createdAt[2]}${issue.createdAt[3]}</p>
                 </div>
             </div>
         `
@@ -113,6 +122,7 @@ document.getElementById("btn-search").addEventListener("click", () => {
     // console.log(inputValue);
     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${inputValue}`;
     fetch(url).then(res => res.json()).then(data => displayIssues(data.data));
+    input.value = "";
 });
 
 loadAllIssues();
