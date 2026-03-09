61e7341c65618352d91af38dc31686f7b9527d84
raf0670 <swagbondhu@gmail.com>
2026-03-09 11:36:20 +0600

    modal added

--- js/home.js
+++ js/home.js
@@ -62,6 +62,59 @@ const loadAllIssues = () => {
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues").then(res => res.json()).then(json => displayIssues(json.data));
 };
 
+const displayIssueDetails = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    const data = details.data;
+    // console.log(data);
+    
+    const container = document.getElementById("modal-container");
+    container.innerHTML = `
+        <div class="p-4 rounded-md space-y-4">
+        <div class="space-y-2">
+            <h1 class="text-3xl font-bold">${data.title}</h1>
+
+            <div class="flex gap-2 items-center">
+                ${data.status === "open" ? `<p class="bg-green-400 text-white text-[12px] font-semibold px-1 rounded-full">OPEN</p>` : `<p class="bg-red-400 text-white text-[12px] font-semibold px-1 rounded-full">CLOSED</p>`}</p>
+                <div class="bg-gray-400 h-1 w-1 rounded-full"></div>
+                <p class="text-gray-400">Opened by ${data.author}</p>
+                <div class="bg-gray-400 h-1 w-1 rounded-full"></div>
+                <p class="text-gray-400">
+                    ${data.createdAt[5]}${data.createdAt[6]}/${data.createdAt[8]}${data.createdAt[9]}/${data.createdAt[0]}${data.createdAt[1]}${data.createdAt[2]}${data.createdAt[3]}
+                </p>
+            </div>
+        </div>
+
+        <div class="flex flex-wrap gap-1 text-[12px] font-semibold">
+            ${data.labels.length === 2 ? `<h2 class="bg-red-100 px-1 py-1 rounded-full text-red-600"><span><i
+                        class="fa-solid fa-bug"></i></span> ${data.labels[0]}</h2>
+            <h2 class="bg-yellow-100 px-1 py-1 rounded-full text-yellow-600"><span><i
+                        class="fa-regular fa-life-ring"></i></span> ${data.labels[1]}</h2>` : `<h2
+                class="bg-green-100 px-1 py-1 rounded-full text-green-600"><span><i class="fa-solid fa-bug"></i></span>
+                ${data.labels[0]}</h2>`}
+        </div>
+
+        <div>
+            <p class="text-gray-400">${data.description}</p>
+        </div>
+
+        <div class="bg-gray-100 rounded-xl w-full p-4 flex gap-2.5">
+            <div class="w-full">
+                <p class="text-gray-400">Assignee:</p>
+                <p>${data.assignee === "" ? `Not Assigned Yet` : data.assignee}</p>
+            </div>
+            <div class="w-full">
+                <p class="text-gray-400">Priority:</p>
+                ${data.priority === "high" ? `<h2 class="bg-red-400 rounded-full w-20 text-center text-xs font-semibold p-1 text-white">HIGH</h2>` : data.priority === "medium" ? `<h2 class="bg-yellow-400 rounded-full w-20 text-center text-xs font-semibold p-1 text-white">MEDIUM</h2>` : `<h2 class="bg-gray-400 rounded-full w-20 text-center text-xs font-semibold p-1 text-white">LOW</h2>`}
+            </div>
+        </div>
+    </div>
+    `;
+
+    document.getElementById("issue_modal").showModal();
+};
+
 const displayIssues = (issues) => {
     // get container
     const container = document.getElementById("card-container");
@@ -83,7 +136,7 @@ const displayIssues = (issues) => {
     issues.forEach(issue => {
         const card = document.createElement("div");
         card.innerHTML = `
-            <div id="card-${issue.id}" class="${issue.status === "open" ? `border-t-3 border-t-green-600` : `border-t-3 border-t-purple-600`} h-full flex flex-col justify-center bg-white shadow-md rounded-lg p-4 space-y-4">
+            <div onclick="displayIssueDetails(${issue.id})" id="card-${issue.id}" class="${issue.status === "open" ? `border-t-3 border-t-green-600` : `border-t-3 border-t-purple-600`} h-full flex flex-col justify-center bg-white shadow-md rounded-lg p-4 space-y-4">
                 <div class="flex justify-between items-center">
                     ${issue.status === "open" ? `<img src="./assets/Open-Status.png" class="" alt="">` : `<img src="./assets/Closed- Status .png" class="" alt="">`}
                     ${issue.priority === "high" ? `<h2 class="bg-red-100 rounded-full w-20 text-center text-xs font-semibold p-1 text-red-600">HIGH</h2>` : issue.priority === "medium" ? `<h2 class="bg-yellow-100 rounded-full w-20 text-center text-xs font-semibold p-1 text-yellow-600">MEDIUM</h2>` : `<h2 class="bg-[#EEEFF2] rounded-full w-20 text-center text-xs font-semibold p-1 text-gray-600">LOW</h2>`}
